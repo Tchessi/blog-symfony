a26191995b81307b9b3043f44482d60862fe53b2
Tchessi <tchesspreza@yahoo.fr>
2022-09-25 00:32:34 +0200

    Home page, dashborad and navbar stylesheet

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,13 @@
-
 import '../css/app.scss';
+import { Dropdown } from 'bootstrap';
 
+document.addEventListener('DOMContentLoaded' ? listner : () => {
+	enableDropDowns();
+})
+
+const enableDropDowns = () => {
+	const dropdownElementList = document.querySelectorAll('.dropdown-toggle');
+	const dropdownList = [...dropdownElementList].map(
+		(dropdownToggleEl) => new Dropdown(dropdownToggleEl)
+	);
+};
