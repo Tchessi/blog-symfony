e30d2179440742f3612085aea0d434a401efad3a
Tchessi <tchesspreza@yahoo.fr>
2022-10-24 13:11:01 +0200

    Amélioration des sustème de connextion

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,6 +8,7 @@ use App\Entity\Comment;
 use App\Entity\Category;
 use Symfony\Component\HttpFoundation\Response;
 use App\Controller\Admin\ArticleCrudController;
+use App\Entity\User;
 use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -43,20 +44,35 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToRoute('Retour vers le site', 'fa fa-undo', 'app_home');
-        yield MenuItem::subMenu('Articles', 'fas fa-newspaper')->setSubItems([
-            MenuItem::linkToCrud( 'Tous les articles', 'fas fa-newspaper', Article::class ),
-            MenuItem::linkToCrud( 'Ajouter', 'fas fa-plus', Article::class )->setAction(Crud::PAGE_NEW),
-            MenuItem::linkToCrud( 'Catégories', 'fas fa-list', Category::class )
-        ]);
 
-        yield MenuItem::subMenu('Menus', 'fas fa-list')->setSubItems([
-            MenuItem::linkToCrud( 'Pages', 'fas fa-file', Menu::class ),
-            MenuItem::linkToCrud( 'Articles', 'fas fa-newspaper', Menu::class ),
-            MenuItem::linkToCrud( 'Liens personnalisés', 'fas fa-link', Menu::class ),
-            MenuItem::linkToCrud( 'Catégories', 'fab fa-delicious', Menu::class ),
+        if ($this->isGranted('ROLE_ADMIN')) {
+
+            yield MenuItem::subMenu('Menus', 'fas fa-list')->setSubItems([
+                MenuItem::linkToCrud( 'Pages', 'fas fa-file', Menu::class ),
+                MenuItem::linkToCrud( 'Articles', 'fas fa-newspaper', Menu::class ),
+                MenuItem::linkToCrud( 'Liens personnalisés', 'fas fa-link', Menu::class ),
+                MenuItem::linkToCrud( 'Catégories', 'fab fa-delicious', Menu::class ),
 
         ]);
+        }
 
-        yield MenuItem::linkToCrud('Commentaires', 'fas fa-comment', Comment::class);
-    }
+        if ($this->isGranted('ROLE_AUTHOR')) {
+
+            yield MenuItem::subMenu('Articles', 'fas fa-newspaper')->setSubItems([
+                MenuItem::linkToCrud( 'Tous les articles', 'fas fa-newspaper', Article::class ),
+                MenuItem::linkToCrud( 'Ajouter', 'fas fa-plus', Article::class )->setAction(Crud::PAGE_NEW),
+                MenuItem::linkToCrud( 'Catégories', 'fas fa-list', Category::class )
+        ]);
+        }
+
+        if ($this->isGranted('ROLE_ADMIN')) {
+            yield MenuItem::linkToCrud('Commentaires', 'fas fa-comment', Comment::class);
+
+            yield MenuItem::subMenu('Comptes', 'fas fa-user')->setSubItems([
+                MenuItem::linkToCrud( 'Tous les comptes', 'fas fa-user-freinds ', User::class ),
+                MenuItem::linkToCrud( 'Ajouter', 'fas fa-plus', User::class )->setAction(Crud::PAGE_NEW)
+        ]);
+        }
+        
+    }   
 }   
